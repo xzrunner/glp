dd7002d3d28de0d5222880e77386874c6fab1958
zz <zhuguang@ejoy.com>
2016-11-01 17:07:50 +0800

    up android clock

--- glp_clock_android.c
+++ glp_clock_android.c
@@ -1,24 +1,48 @@
 #include "glp_clock.h"
 
+#include <time.h>
+
+struct glp_clock {
+	struct timespec res;
+	uint64_t start, last;
+};
+
 struct glp_clock* 
 glp_clock_create() {
-    return NULL;
+	struct glp_clock* clk = (struct glp_clock*)malloc(sizeof(*clk));
+	memset(clk, 0, sizeof(*clk));
+	return clk;
 }
 
 void 
 glp_clock_release(struct glp_clock* clk) {
+	free(clk);
+}
+
+static uint64_t
+_get_absolute_time(struct glp_clock* clk) {
+	clock_gettime(CLOCK_REALTIME, &clk->res);
+	return 1000.0 * clk->res.tv_sec + (double) clk->res.tv_nsec / 1e6;
 }
 
 void 
 glp_clock_start(struct glp_clock* clk) {
+	clk->start = _get_absolute_time(clk);
+	clk->last = clk->start;
 }
 
 uint32_t 
 glp_clock_get_time(struct glp_clock* clk) {
-	return 0;
+	uint64_t tmp = _get_absolute_time(clk);
+	return tmp - clk->start;
 }
 
 uint32_t 
 glp_clock_get_during(struct glp_clock* clk, bool reset) {
-	return 0;
+	uint64_t tmp = _get_absolute_time(clk);
+	uint32_t during = tmp - clk->last;
+	if (reset) {
+		clk->last = tmp;
+	}
+	return during;
 }
